b7e938fef1d27c03e0667ef5a1b4748d54b96aa6
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-06-28 00:53:32 +0530

    added guess processing functionality

--- guessplay/src/main.rs
+++ guessplay/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hurry ! guess a number quick !!");
+    println!("Now quickly enter what you have guessed !");
+    
+    let mut myguess = String::new();
+
+    io::stdin()
+	.read_line(&mut myguess)
+	.ok()
+	.expect("can not read !! I am blind again !!");
+    
+    println!("You guessed: {}", myguess);
 }
