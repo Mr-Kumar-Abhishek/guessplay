cf7430c93dbd25accd1410a015f45a2c1b9c094f
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-06-28 01:33:17 +0530

    random number generation

--- guessplay/src/main.rs
+++ guessplay/src/main.rs
@@ -1,7 +1,10 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Hurry ! guess a number quick !!");
+    println!("Hurry ! guess a number between 1 and 100 quick !!");
     println!("Now quickly enter what you have guessed !");
     
     let mut myguess = String::new();
@@ -12,4 +15,8 @@ fn main() {
 	.expect("can not read !! I am blind again !!");
     
     println!("You guessed: {}", myguess);
+    
+    let secret = rand::thread_rng().gen_range(1,101);
+    println!("The secret number is: {}", secret);
+
 }
