8df208b9ef4fbe66b2b533b9ded2e702b910743b
Abhishek Kumar <mr.kumar.abhishek@email.com>
2015-06-28 13:27:14 +0530

    added loop for multiple guesses

--- guessplay/src/main.rs
+++ guessplay/src/main.rs
@@ -5,31 +5,36 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Hurry ! guess a number between 1 and 100 quick !!");
-    println!("Now quickly enter what you have guessed !");
+	loop {
+    	println!("Hurry ! guess a number between 1 and 100 quick !!");
+    	println!("Now quickly enter what you have guessed !");
     
-    let mut myguess = String::new();
+    	let mut myguess = String::new();
 
-    io::stdin()
-	.read_line(&mut myguess)
-	.ok()
-	.expect("can not read !! I am blind again !!");
+    	io::stdin()
+			.read_line(&mut myguess)
+			.ok()
+			.expect("can not read !! I am blind again !!");
     
-    let myguess: u32 = myguess
-	.trim()
-	.parse()
-	.ok()
-	.expect("huh ? type a number");
+    	let myguess: u32 = match myguess
+			.trim()
+			.parse(){
+				Ok(num) => num,
+				Err(_) => continue,
+			};
 
-    println!("You guessed: {}", myguess);
+    	println!("You guessed: {}", myguess);
     
-    let secret = rand::thread_rng().gen_range(1,101);
-    match myguess.cmp(&secret) {
-	Ordering::Less => println!("Too small !!"),
-	Ordering::Greater => println!("Too BIG !!"),
-	Ordering::Equal => println!("You Win !!!"),
+    	let secret = rand::thread_rng().gen_range(1,101);
+    	match myguess.cmp(&secret) {
+			Ordering::Less => println!("Too small !!"),
+			Ordering::Greater => println!("Too BIG !!"),
+			Ordering::Equal => {
+				println!("You Win !!!");
+				break;
+    		}
+    	}
+
+    	println!("The secret number is: {}", secret);
     }
-
-    println!("The secret number is: {}", secret);
-
 }
